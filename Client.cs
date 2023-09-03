45586a1072cbf27b8e0cfab728c51f72f9a44984
ledepede1 <142742362+ledepede1@users.noreply.github.com>
2023-09-03 14:05:46 +0200

    Update Client.cs

--- Client.cs
+++ Client.cs
@@ -1,1 +1,16 @@
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using System;
+using System.Collections.Generic;
 
+namespace Main {
+    public class Main : BaseScript {
+        public static void Command() {
+            API.RegisterCommand("getmoney", new Action<int, List<object>, string>((source, args, rawCommand) =>
+            {
+                int amount = 1000;
+                TriggerServerEvent("('esxGuide:AddMoney", amount);
+            }), false);
+        }
+    }
+}
