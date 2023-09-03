599149a87f79f4b3a6fb476a8695bd8e26eabbfa
ledepede1 <142742362+ledepede1@users.noreply.github.com>
2023-09-03 14:03:44 +0200

    Update Server.lua

--- Server.lua
+++ Server.lua
@@ -1,1 +1,13 @@
+ESX = nil
 
+TriggerEvent('esx:getSharedObject', function(obj)
+    ESX = obj
+end)
+
+RegisterServerEvent('esxGuide:AddMoney')
+AddEventHandler('esxGuide:AddMoney', function(amount)
+
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    xPlayer.addMoney(amount) 
+end)
